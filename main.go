c355e19d80c7183f42fc4e535ab9570a71178acf
Toyoharu Han <toyoh3232d@protonmail.com>
2020-10-22 12:10:47 +0900

    add p33

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	p "leetcode/p52"
+	p "leetcode/p33"
 )
 
 func main() {
--- p33/search-in-rotated-sorted-array.go
+++ p33/search-in-rotated-sorted-array.go
@@ -8,13 +8,39 @@ package p33
 
 //Test ...
 func Test() int {
-	nums, target := []int{4, 5, 6, 7, 0, 1, 2}, 0
+	nums, target := []int{4, 5, 6, 7, 0, 1, 2}, 3
 	return search(nums, target)
 }
 
 // @lc code=start
 func search(nums []int, target int) int {
-	return 0
+	i, j := 0, len(nums)-1
+	for i <= j {
+		mid := (i + j) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[i] < nums[mid] {
+			switch {
+			case i <= mid-1 && nums[i] <= target && target <= nums[mid-1]:
+				j = mid - 1
+				continue
+			default:
+				i = mid + 1
+				continue
+			}
+		} else {
+			switch {
+			case mid+1 <= j && nums[mid+1] <= target && target <= nums[j]:
+				i = mid + 1
+				continue
+			default:
+				j = mid - 1
+				continue
+			}
+		}
+	}
+	return -1
 }
 
 // @lc code=end
